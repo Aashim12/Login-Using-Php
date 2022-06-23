5c54554ed382fb36210678fd665a08c93b4270ac
Aashim Singh <aashimsingh74@gmail.com>
2022-06-23 14:08:16 +0530

    changes

--- admin.php
+++ admin.php
@@ -4,25 +4,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="admin.css?v=<?php echo time(); ?>">
+
 </head>
 <body>
 
 <?php
 require('db.php');
-
 $query=mysqli_query($conn," SELECT * FROM userlogin ");
 ?>
-<div class="conatiner">
-<table border="1" cellspacing="0" cellpadding="10">
+<div class="container">
+<table class= "table table-striped table-dark" id="admin-table">
   <tr>
-    <th>S.N</th>
-    <th>Full Name</th>
-    <th>Gender</th>
+    <th scope ="row">S.N</th>
+    <th>First Name</th>
     <th>Email</th>
+    <th>Username</th>   
     <th>Mobile No</th>
     <th>Date of birth</th>
-    <!-- <th>City</th>
-    <th>State </th> -->
   </tr>
 <?php
       if(mysqli_num_rows($query)>0){
@@ -31,17 +31,16 @@ $query=mysqli_query($conn," SELECT * FROM userlogin ");
         ?>
            <td><?php echo $sn; ?> </td>
    <td><?php echo $data['firstname']; ?> </td>
-   <td><?php echo $data['lastname']; ?> </td>
+   <!-- <td><?php echo $data['lastname']; ?> </td> -->
    <td><?php echo $data['email']; ?> </td>
+   <td><?php echo $data['user']; ?> </td>
    <td><?php echo $data['phoneno']; ?> </td>
    <td><?php echo $data['datebirth']; ?> </td>
-   <!-- <td><?php echo $data['']; ?> </td> -->
-   <!-- <td><?php echo $data['state']; ?> </td> -->
  <tr>
  <?php
   $sn++;} } else { ?>
 <tr>
-     <td colspan="8">No data found</td>
+     <td colspan="6">No data found</td>
     </tr>
  <?php } ?>
   </table>
